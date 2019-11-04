d933fb8ebec90641aa15de4ad814170f233ec221
Daniel <43273980+dbistuer@users.noreply.github.com>
2019-11-04 19:41:50 +0100

    ADD PAREL_SENAR

--- ex2.c
+++ ex2.c
@@ -8,10 +8,13 @@
 #define PARELL_SENAR 2
 #define NO_JUGAR 0
 #define CAPITAL_INICIAL 1000
+#define PARELL 2
+#define SENAR 1
+
 void mostraMenu();
 void clrscr();
 void simulacioCarrega();
-int seleccio = -1,numero_escollit = -1;
+int seleccio = -1,numero_escollit = -1,monedero = CAPITAL_INICIAL;
 int numero_ruleta = 0;
 
 int main() {
@@ -27,37 +30,60 @@ int main() {
 
     while (seleccio != NO_JUGAR)
     {
-        if(seleccio == ENDEVINAR || seleccio == PARELL_SENAR){
-            srand(time(NULL)); // Inicialitzaci´o, nom´es s’ha de fer una vegada al principi
-            if(seleccio == ENDEVINAR){
-                printf("Insereix el numero escollit entre [%i-%i] : ",MIN_RULETA,MAX_RULETA);
+        srand(time(NULL)); // Inicialitzaci´o, nom´es s’ha de fer una vegada al principi
+        if(seleccio == ENDEVINAR){
+            printf("Insereix el numero escollit entre [%i-%i] : ",MIN_RULETA,MAX_RULETA);
+            scanf("%i",&numero_escollit);
+            while(numero_escollit<MIN_RULETA && numero_escollit>MAX_RULETA){
+                printf("\nEl numero escollit no es correcte!!\n\n Ha de ser un numero entre %i i %i");
+                printf("Torna a inserir el numero: ");
                 scanf("%i",&numero_escollit);
-                while(numero_escollit<MIN_RULETA && numero_escollit>MAX_RULETA){
-                    printf("\nEl numero escollit no es correcte!!\n\n Ha de ser un numero entre %i i %i");
-                    printf("Torna a inserir el numero: ");
-                    scanf("%i",&numero_escollit);
-                }
-                simulacioCarrega();
-                numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
-                if(numero_ruleta == numero_escollit){
-                    printf("Has guanyat!!!!");
-                    printf("El numero ha sigut: %i i el te unmero es: %i",numero_ruleta,numero_escollit);
-                }else{
-                    printf("Has perdut pringao!!!!");
-                    printf("El numero ha sigut: %i i el te unmero es: %i",numero_ruleta,numero_escollit);
-                }
-
-            }else //Com ja s'ha comprovat anteriorment que sol es pot seleccionar 'ENDEVINAR' o 'PARELL_SENAR' else es correcte
-            {
-                /* code */
+            }
+            simulacioCarrega();
+            numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
+            if(numero_ruleta == numero_escollit){
+                printf("Has guanyat!!!!");
+                printf("El numero ha sigut: %i i el te unmero es: %i",numero_ruleta,numero_escollit);
+            }else{
+                printf("Has perdut pringao!!!!");
+                printf("El numero ha sigut: %i i el te unmero es: %i",numero_ruleta,numero_escollit);
             }
 
+        }else //Com ja s'ha comprovat anteriorment que sol es pot seleccionar 'ENDEVINAR' o 'PARELL_SENAR' else es correcte
+        {
+            printf("Has seleccionat Parell o Senar\n");
+            printf("---------------------------------\n\n");
+            numero_escollit = -1;
+            while(numero_escollit != PARELL && numero_escollit != SENAR){
+                printf("Si vols apostar a Parell insereix %i, altrament si vols Senar insereix %i.\n",PARELL,SENAR);
+                scanf("%i",&numero_escollit);
+            }
+            if(numero_escollit == PARELL)
+                printf("Has escollit PARELLS");
+            else
+                printf("Has escollit SENARS");
+            simulacioCarrega();
+            numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
+            if(numero_ruleta % 2 == 0){//parells
+                if(numero_escollit == PARELL){
+                    printf("Has guanyat");
+                }else{
+                    printf("Has perdut pringao");
+                }
+            }else{ //senars
+                if(numero_escollit == SENAR){
+                    printf("Has guanyat");
+                }else{
+                    printf("Has perdut pringao");
+                }
+            }
         }
     }
 
     return 0;
 }
 
+//Mostra les opcions de joc
 void mostraMenu(){
     printf(" - - - - - - - - - - - - \n");
     printf("| Endevinar numero : %i  |\n",ENDEVINAR);
