4a957fae5109c97aa06398e9f06a279d855e7ad5
Daniel <43273980+dbistuer@users.noreply.github.com>
2019-11-18 21:22:35 +0100

    fet

--- ex2.c
+++ ex2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #define MIN_RULETA 1
 #define PAUSA 2
 #define MAX_RULETA 36
@@ -11,150 +12,174 @@
 #define PARELL 2
 #define SENAR 1
 
-void mostraMenu();
-void clrscr();
-void simulacioCarrega();
-int seleccio = -1,numero_escollit = -1,monedero = CAPITAL_INICIAL;
-int numero_ruleta = 0;
+void mostraMenu(int monedero);
+void clrscr(bool ubuntu);
+int seleccioJoc(bool ubuntu, int seleccio,int monedero);
+int jocEndevinar(bool ubuntu, int numero_escollit,int numero_ruleta,int aposta,int *patides_guanyades,int *patides_perdudes);
+int jocParellSenar(bool ubuntu,int numero_escollit,int numero_ruleta,int aposta,int *patides_guanyades,int *patides_perdudes);
+void seleccioSistemaOperatiu(bool *ubuntu);
+void cantitatAposta(bool ubuntu,int *aposta,int monedero);
+
 
 int main() {
 
-    while(seleccio != NO_JUGAR && seleccio != ENDEVINAR && seleccio != PARELL_SENAR){
-        if(seleccio != -1)
-            printf("\nEl numero insertat no es correcte!!!!\n\n\n");
-        mostraMenu();
-        printf("Insereix el numero del que vols fer: ");
-        scanf("%i",&seleccio);
-        clrscr();
-    }
+// Variables
+int
+    seleccio = -1,
+    numero_escollit = -1,
+    monedero = CAPITAL_INICIAL,
+    numero_ruleta = 0,
+    aposta = 0,
+    patides_guanyades = 0,
+    patides_perdudes = 0;
+bool ubuntu = true;
+char pause;
+//---------------------------
 
-    while (seleccio != NO_JUGAR)
+    srand(time(NULL)); // Inicialitzaci´o, nom´es s’ha de fer una vegada al principi
+    seleccioSistemaOperatiu(&ubuntu);
+    while (seleccio != NO_JUGAR && monedero > 0)
     {
-        srand(time(NULL)); // Inicialitzaci´o, nom´es s’ha de fer una vegada al principi
-        if(seleccio == ENDEVINAR){
-            printf("Insereix el numero escollit entre [%i-%i] : ",MIN_RULETA,MAX_RULETA);
-            scanf("%i",&numero_escollit);
-            while(numero_escollit<MIN_RULETA && numero_escollit>MAX_RULETA){
-                printf("\nEl numero escollit no es correcte!!\n\n Ha de ser un numero entre %i i %i");
-                printf("Torna a inserir el numero: ");
-                scanf("%i",&numero_escollit);
-            }
-            simulacioCarrega();
-            numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
-            if(numero_ruleta == numero_escollit){
-                printf("Has guanyat!!!!");
-                printf("El numero ha sigut: %i i el te unmero es: %i",numero_ruleta,numero_escollit);
-            }else{
-                printf("Has perdut pringao!!!!");
-                printf("El numero ha sigut: %i i el te unmero es: %i",numero_ruleta,numero_escollit);
-            }
+        seleccio = seleccioJoc(ubuntu,&seleccio,monedero);
+        if (seleccio != NO_JUGAR){
+            cantitatAposta(ubuntu,&aposta,monedero);
 
-        }else{
-            printf("Has seleccionat Parell o Senar\n");
-            printf("---------------------------------\n\n");
-            numero_escollit = -1;
-            while(numero_escollit != PARELL && numero_escollit != SENAR){
-                printf("Si vols apostar a Parell insereix %i, altrament si vols Senar insereix %i.\n",PARELL,SENAR);
-                scanf("%i",&numero_escollit);
-            }
-            if(numero_escollit == PARELL)
-                printf("Has escollit PARELLS");
-            else
-                printf("Has escollit SENARS");
-            simulacioCarrega();
-            numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
-            if(numero_ruleta % 2 == 0){//parells
-                if(numero_escollit == PARELL){
-                    printf("Has guanyat");
-                }else{
-                    printf("Has perdut pringao");
-                }
-            }else{ //senars
-                if(numero_escollit == SENAR){
-                    printf("Has guanyat");
-                }else{
-                    printf("Has perdut pringao");
-                }
+            if(seleccio == ENDEVINAR)
+                monedero += jocEndevinar(ubuntu,numero_escollit,numero_ruleta,aposta,&patides_guanyades,&patides_perdudes);
+            else if(seleccio == PARELL_SENAR)
+                monedero += jocParellSenar(ubuntu,numero_escollit,numero_ruleta,aposta,&patides_guanyades,&patides_perdudes);
+            if(monedero != 0){
+                printf("\n\n\n\n\nPolsa ENTER per a continuar.");
+                while(getchar() != '\n');
+                scanf("%[^\n]",&pause);
             }
         }
     }
+    if(monedero == 0){
+        printf("\nHo has perdut tot!\n\n");
+    }else{
+        printf("El teu capital final es de %i Euros.\n",monedero);
+    }
+    printf("Has guanyat %i partides i n'has perdut %i\n\n",patides_guanyades,patides_perdudes);
 
     return 0;
 }
 
+void seleccioSistemaOperatiu(bool *ubuntu){
+    int sistema = -1;
+    do{
+        printf("Benvingut al joc de la ruleta!\n\n");
+        printf("Insereix 0 si el sistema es Ubuntu o 1 si es Windows:\n");
+        scanf("%i",&sistema);
+    }while(sistema<0 && sistema<1);
+    clrscr(ubuntu);
+    if(sistema == 0)
+        *ubuntu = true;
+    else
+        *ubuntu = false;
+}
+
+void cantitatAposta(bool ubuntu,int *aposta,int monedero){
+    int auxAposta = 0;
+    *aposta = 0;
+    while(*aposta < 1 || *aposta > monedero){
+        printf("Tens un capital de %i Euros, quina quantitat vols apostar?\n",monedero);
+        scanf("%i",&auxAposta);
+        *aposta = auxAposta;
+        clrscr(ubuntu);
+        if(*aposta < 1 || *aposta > monedero){
+            if(*aposta < 1){
+                printf("L'aposta a realitzar no pot ser menor que 1.\n\n");
+            }else if(*aposta > monedero){
+                printf("L'aposta no pot ser mes gran que el capital que es disposa.\n\n");
+            }
+        }
+    }
+}
+
+int jocParellSenar(bool ubuntu,int numero_escollit,int numero_ruleta,int aposta,int *patides_guanyades,int *patides_perdudes){
+            numero_escollit = -1;
+            while(numero_escollit != PARELL && numero_escollit != SENAR){
+                printf("Prem %i per apostar a numero senar o %i per apostar a numero parell:\n",SENAR,PARELL);
+                scanf("%i",&numero_escollit);
+            }
+            numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
+            clrscr(ubuntu);
+            if(numero_escollit == PARELL)
+                printf("\nHas apostat per parell i ha sortit %i\n",numero_ruleta);
+            else
+                printf("\nHas apostat per senar i ha sortit %i\n",numero_ruleta);
+            if(numero_ruleta % 2 == 0){//parells
+                if(numero_escollit == PARELL){
+                    printf("Has guanyat %i Euros!",aposta);
+                    *patides_guanyades += 1;
+                    return aposta;
+                }else{
+                    printf("Has perdut %i Euros!",aposta);
+                    *patides_perdudes += 1;
+                    return -aposta;
+                }
+            }else{ //senars
+                if(numero_escollit == SENAR){
+                    printf("Has guanyat %i Euros!",aposta);
+                    *patides_guanyades += 1;
+                    return aposta;
+                }else{
+                    printf("Has perdut %i Euros!",aposta);
+                    *patides_perdudes += 1;
+                    return -aposta;
+                }
+            }
+}
+
+int jocEndevinar(bool ubuntu, int numero_escollit,int numero_ruleta,int aposta,int *patides_guanyades,int *patides_perdudes){
+    printf("Introdueix el numero per la teva aposta (de l’%i al %i) : \n",MIN_RULETA,MAX_RULETA);
+    scanf("%i",&numero_escollit);
+    while(numero_escollit<MIN_RULETA || numero_escollit>MAX_RULETA){
+        clrscr(ubuntu);
+        printf("El numero escollit no es correcte!!\n\n Ha de ser un numero entre %i i %i\n\n",MIN_RULETA,MAX_RULETA);
+        printf("Torna a inserir el numero: ");
+        scanf("%i",&numero_escollit);
+    }
+    numero_ruleta = rand() % MAX_RULETA + MIN_RULETA; // Genera n´umeros de l’1 al 36
+    clrscr(ubuntu);
+    printf("Has apostat pel numero %i i ha sortit %i\n",numero_escollit,numero_ruleta);
+    if(numero_ruleta == numero_escollit){
+        printf("Has guanyat %i Euros!",aposta);
+        *patides_guanyades += 1;
+        return aposta;
+    }else{
+        printf("Has perdut %i Euros!",aposta);
+        *patides_perdudes += 1;
+        return -aposta;
+    }
+}
+
+int seleccioJoc(bool ubuntu, int seleccio,int monedero){
+    seleccio = -1;
+    clrscr(ubuntu);
+    while(seleccio != NO_JUGAR && seleccio != ENDEVINAR && seleccio != PARELL_SENAR){
+            if(seleccio != -1)
+                printf("\nEl numero insertat no es correcte!!!!\n\n\n");
+            mostraMenu(monedero);
+            scanf("%i",&seleccio);
+            clrscr(ubuntu);
+        }
+        return seleccio;
+}
+
 //Mostra les opcions de joc
-void mostraMenu(){
-    printf(" - - - - - - - - - - - - \n");
-    printf("| Endevinar numero : %i  |\n",ENDEVINAR);
-    printf("| Parell/Senar : %i      |\n",PARELL_SENAR);
-    printf("| Sortir : %i            |\n",NO_JUGAR);
-    printf(" - - - - - - - - - - - - \n\n");
+void mostraMenu(int monedero){
+    printf("Tens un capital de %i Euros. A quin joc vols jugar?\n",monedero);
+    printf("Prem %i per jugar a endevinar el numero de l’%i al %i\n",ENDEVINAR,MIN_RULETA,MAX_RULETA);
+    printf("Prem %i per jugar a endevinar si sera parell o senar\n",PARELL_SENAR);
+    printf("Prem %i per sortir\n",NO_JUGAR);
 }
 
 //Neteja la pantalla
-void clrscr()
-{
-    system("@cls||clear");
-}
-
-//Simula una carrega i fa esperar a l'usuari una mica
-void simulacioCarrega(){
-    printf("\n\n\n\n\n                    |                           |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-                          |");
-    clrscr();
-    printf("\n\n\n\n\n                    |--                         |");
-    clrscr();
-    printf("\n\n\n\n\n                    |---                        |");
-    clrscr();
-    printf("\n\n\n\n\n                    |----                       |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-----                      |");
-    clrscr();
-    printf("\n\n\n\n\n                    |------                     |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-------                    |");
-    clrscr();
-    printf("\n\n\n\n\n                    |--------                   |");
-    clrscr();
-    printf("\n\n\n\n\n                    |---------                  |");
-    clrscr();
-    printf("\n\n\n\n\n                    |----------                 |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-----------                |");
-    clrscr();
-    printf("\n\n\n\n\n                    |------------               |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-------------              |");
-    clrscr();
-    printf("\n\n\n\n\n                    |--------------             |");
-    clrscr();
-    printf("\n\n\n\n\n                    |---------------            |");
-    clrscr();
-    printf("\n\n\n\n\n                    |----------------           |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-----------------          |");
-    clrscr();
-    printf("\n\n\n\n\n                    |------------------         |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-------------------        |");
-    clrscr();
-    printf("\n\n\n\n\n                    |--------------------       |");
-    clrscr();
-    printf("\n\n\n\n\n                    |---------------------      |");
-    clrscr();
-    printf("\n\n\n\n\n                    |----------------------     |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-----------------------    |");
-    clrscr();
-    printf("\n\n\n\n\n                    |------------------------   |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-------------------------  |");
-    clrscr();
-    printf("\n\n\n\n\n                    |-------------------------- |");
-    clrscr();
-    printf("\n\n\n\n\n                    |---------------------------|");
-    clrscr();
-    printf("\n\n\n\n\n                    D  O  N  E  ! ! !");
-    clrscr();
+void clrscr(bool ubuntu){
+    if(ubuntu)
+        system("clear");
+    else
+        system("@cls");
 }
